055d047562ad057fc12c17c5ac8359b3e01395b2
nastyheatnor <nastyheatnor@hotmail.com>
2018-12-19 16:54:25 -0500

    Test

--- www/js/index.js
+++ www/js/index.js
@@ -33,66 +33,45 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        console.log('Received Device Ready Event');
-        console.log('calling setup push');
-        app.setupPush();
+        app.receivedEvent('deviceready');
 		window.plugins.PushbotsPlugin.initialize("5c19b4970540a31b78243ed2", {"android":{"sender_id":"633747534823"}});
+
 		// Only with First time registration
 		window.plugins.PushbotsPlugin.on("registered", function(token){
 			console.log("Registration Id:" + token);
 		});
-		
+
 		//Get user registrationId/token and userId on PushBots, with evey launch of the app even launching with notification
 		window.plugins.PushbotsPlugin.on("user:ids", function(data){
 			console.log("user:ids" + JSON.stringify(data));
 		});
     },
-    setupPush: function() {
-        console.log('calling push init');
-        var push = PushNotification.init({
-            "android": {
-                "senderID": "633747534823"
-            },
-            "browser": {},
-            "ios": {
-                "sound": true,
-                "vibration": true,
-                "badge": true
-            },
-            "windows": {}
-        });
-        console.log('after init');
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
-        push.on('registration', function(data) {
-            console.log('registration event: ' + data.registrationId);
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
 
-            var oldRegId = localStorage.getItem('registrationId');
-            if (oldRegId !== data.registrationId) {
-                // Save new registration ID
-                localStorage.setItem('registrationId', data.registrationId);
-                // Post registrationId to your app server as the value has changed
-            }
-
-            var parentElement = document.getElementById('registration');
-            var listeningElement = parentElement.querySelector('.waiting');
-            var receivedElement = parentElement.querySelector('.received');
-
-            listeningElement.setAttribute('style', 'display:none;');
-            receivedElement.setAttribute('style', 'display:block;');
-        });
-
-        push.on('error', function(e) {
-            console.log("push error = " + e.message);
-        });
-
-        push.on('notification', function(data) {
-            console.log('notification event');
-            navigator.notification.alert(
-                data.message,         // message
-                null,                 // callback
-                data.title,           // title
-                'Ok'                  // buttonName
-            );
-       });
+        console.log('Received Event: ' + id);
     }
 };
+
+// Should be called once app receive the notification [foreground/background]
+window.plugins.PushbotsPlugin.on("notification:received", function(data){
+	console.log("received:" + JSON.stringify(data));
+	
+	//iOS: [foreground/background]
+	console.log("notification received from:" + data.cordova_source);
+	//Silent notifications Only [iOS only]
+	//Send CompletionHandler signal with PushBots notification Id
+	window.plugins.PushbotsPlugin.done(data.pb_n_id);
+});
+
+window.plugins.PushbotsPlugin.on("notification:clicked", function(data){
+	// var userToken = data.token; 
+       // var userId = data.userId;
+  	console.log("clicked:" + JSON.stringify(data));
+});
