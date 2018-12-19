1b374a5499dfc44c13c4a79a29ef3d36091f6cdb
nastyheatnor <nastyheatnor@hotmail.com>
2018-12-19 17:28:07 -0500

    Test

---
+++ node_modules/pushbots-cordova-plugin/src/ios/PushbotsPlugin.h
@@ -0,0 +1,41 @@
+//
+//  Pushbtots.h
+//  Pushbots Cordova plugin
+//
+#import "AppDelegate.h"
+#import <Cordova/CDV.h>
+#import <Pushbots/Pushbots.h>
+
+@interface PushbotsPlugin : CDVPlugin{
+	 void (^completionHandler)(UIBackgroundFetchResult);
+}
+
+@property NSString *callbackId;
+@property (nonatomic, strong) NSDictionary *notificationPayload;
+@property (nonatomic, strong) NSMutableDictionary *silentHandler;
+
+- (void) initialize:(CDVInvokedUrlCommand*)command;
+
+- (void) updateAlias:(CDVInvokedUrlCommand *)command;
+- (void) removeAlias:(CDVInvokedUrlCommand *)command;
+- (void) update:(CDVInvokedUrlCommand*)command;
+
+- (void) tag:(CDVInvokedUrlCommand*)command;
+- (void) untag:(CDVInvokedUrlCommand*)command;
+- (void) setTags:(CDVInvokedUrlCommand *)command;
+- (void) removeTags:(CDVInvokedUrlCommand *)command;
+
+- (void) trackEvent:(CDVInvokedUrlCommand*)command;
+
+- (void) toggleNotifications:(CDVInvokedUrlCommand *)command;
+- (void) debug:(CDVInvokedUrlCommand*)command;
+- (void) unregister:(CDVInvokedUrlCommand *)command;
+- (void) getRegistrationId:(CDVInvokedUrlCommand *)command;
+
+//iOS Badge
+- (void) clearBadgeCount:(CDVInvokedUrlCommand*)command;
+- (void) setBadge:(CDVInvokedUrlCommand*)command;
+- (void) incrementBadgeCountBy:(CDVInvokedUrlCommand*)command;
+- (void) decrementBadgeCountBy:(CDVInvokedUrlCommand*)command;
+
+@end
