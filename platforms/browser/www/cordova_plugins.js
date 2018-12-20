e9182edf51568978203f3f66d86d11a49ec21229
nastyheatnor <nastyheatnor@hotmail.com>
2018-12-20 15:22:35 -0500

    Thomh

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -1,22 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/phonegap-plugin-push/www/push.js",
-        "id": "phonegap-plugin-push.PushNotification",
-        "pluginId": "phonegap-plugin-push",
-        "clobbers": [
-            "PushNotification"
-        ]
-    },
-    {
-        "file": "plugins/phonegap-plugin-push/www/browser/push.js",
-        "id": "phonegap-plugin-push.BrowserPush",
-        "pluginId": "phonegap-plugin-push",
-        "clobbers": [
-            "PushNotification"
-        ]
-    },
-    {
         "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
         "id": "onesignal-cordova-plugin.OneSignal",
         "pluginId": "onesignal-cordova-plugin",
@@ -28,7 +12,6 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "phonegap-plugin-push": "1.8.4",
     "onesignal-cordova-plugin": "2.4.6"
 }
 // BOTTOM OF METADATA
--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,14 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
-    "id": "phonegap-plugin-push.PushNotification",
-    "file": "plugins/phonegap-plugin-push/www/push.js",
-    "pluginId": "phonegap-plugin-push",
-    "clobbers": [
-      "PushNotification"
-    ]
-  },
-  {
     "id": "onesignal-cordova-plugin.OneSignal",
     "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
     "pluginId": "onesignal-cordova-plugin",
@@ -20,7 +12,6 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "phonegap-plugin-push": "1.8.4",
   "onesignal-cordova-plugin": "2.4.6"
 };
 // BOTTOM OF METADATA
