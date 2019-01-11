7e8fefe6fe4bd9672e1779ef75dfe43b5d2804f7
nastyheatnor <nastyheatnor@hotmail.com>
2019-01-11 16:56:06 -0500

    Thing

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -393,7 +393,8 @@ module.exports.metadata =
   "cordova-plugin-media-capture": "1.4.3",
   "cordova-plugin-network-information": "1.3.4",
   "cordova-plugin-vibration": "2.1.6",
-  "onesignal-cordova-plugin": "2.4.6"
+  "onesignal-cordova-plugin": "2.4.6",
+  "cordova-plugin-whitelist": "1.3.3"
 };
 // BOTTOM OF METADATA
 });
